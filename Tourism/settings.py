344c149a3159c07be83b745cd0d38fdee1d07259
Shaf3y <Shaf3iibrahim@gmail.com>
2019-02-11 19:51:39 +0200

    Configuring settings

--- Tourism/settings.py
+++ Tourism/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,11 @@ WSGI_APPLICATION = 'Tourism.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'Tourism',
+        'USER': 'phpmyadmin',
+        'PASSWORD': 'Os@12345',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
@@ -117,4 +122,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'Admin/static/')
