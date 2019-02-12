fab5b2731a8d2e0b36c1074a18f3432de39559e6
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-02-12 22:47:09 +0200

    create class Car_Rental

---
+++ Car_Rental/models.py
@@ -0,0 +1,21 @@
+'''
+ in this class, there are few assumptions assumed in designing table "Car_Rental".
+ #1 -> name of table that car_Rental references is "Location".
+ #2 -> the foreign key is assumed to be of type CharField (although this is not an issue).
+'''
+
+from django.db import models
+
+
+
+# Create your models here.
+#class representing table for car rentals services
+class Car_Rental (models.Model):
+
+    #setting fields of the table
+    car_rental_id = models.CharField(max_length = 15)
+    car_rental_name = models.CharField(max_length = 45)
+    location_id = models.ForeignKey(Location) # Location class is not defined since it's not inserted
+
+    def __str__(self):
+        return self.rental_id
