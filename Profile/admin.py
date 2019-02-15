e79abef3c411402a4f838d559f9ac054b311a3fa
sheroukRashed <eng_sheroukrashed@yahoo.com>
2019-02-15 14:46:25 +0200

    sherouk sign up and sign in

--- Profile/admin.py
+++ Profile/admin.py
@@ -6,9 +6,9 @@ from .models import User
 
 class CustomModel(admin.ModelAdmin):
         fieldsets = (
-        ['Users_Tabel' , {'fields': [ 'user_name' , 'first_name', 'last_name', 'user_password' , 'user_email']}],
+        ['Users_Tabel' , {'fields': [ 'username' , 'first_name', 'last_name', 'user_password' , 'user_email' , 'blk_flg']}],
         )
-        list_display = ('user_id' ,'user_name' , 'first_name', 'last_name', 'user_password' , 'user_email' , 'blk_flg')
+        list_display = ('user_id' ,'username' , 'first_name', 'last_name', 'user_password' , 'user_email' , 'blk_flg')
 
 
 # Register your models here.
--- Tourism/settings.py
+++ Tourism/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hw%jo35apvljd9!i^ilq5p093r#v@zi1__yeycb-3hhr%i_ecj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 LOGIN_REDIRECT_URL = '/'
 
 # Application definition
